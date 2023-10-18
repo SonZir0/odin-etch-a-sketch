8462c174d490963dbe95f5de69027ade407f1c3b
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2023-10-18 17:32:37 +0400

    Add a function to create/append new divs to color
    Coloring isn't yet implemented. Some adjustments to
    style, most importantly - flexbox to contain new divs.
    Code to set .main dimensions was refactored into
    separate function

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
-let main = document.querySelector('main');
-let grid = document.querySelector('.grid');
+function setGridSizeOnLoad() {
+    let main = document.querySelector('main');
+    let grid = document.querySelector('.grid');
 
-// get height and width available for drawing
-let height = main.clientHeight +
- parseFloat(window.getComputedStyle(main).marginTop) +
- parseFloat(window.getComputedStyle(main).marginBottom);
+    // get height and width available for drawing
+    let height = main.clientHeight +
+        parseFloat(window.getComputedStyle(main).marginTop) +
+        parseFloat(window.getComputedStyle(main).marginBottom);
 
- let width = main.clientWidth +
- parseFloat(window.getComputedStyle(main).marginLeft) +
- parseFloat(window.getComputedStyle(main).marginRight);
+    let width = main.clientWidth +
+        parseFloat(window.getComputedStyle(main).marginLeft) +
+        parseFloat(window.getComputedStyle(main).marginRight);
 
-// our grid is supposed to be a square, so we're taking the minimum value
-let gridLength = Math.floor(Math.min(height, width));
+    // our grid is supposed to be a square, so we're taking the minimum value
+    let gridLength = Math.floor(Math.min(height, width));
 
-// take a bit of lenght for a gap between the border and new divs
-let aBitForPadding = (gridLength % 10) + 10;
-gridLength -= aBitForPadding;
-// + 'px' is necessary for assigning a pixel value
-gridLength += "px";
+    // take a bit (10 + whatever) of lenght for a gap between the border and
+    // new divs save "actual" width/height for later (to not compute later)
+    let aBitForPadding = (gridLength % 10) + 10;
+    // last -10 is from borders on both sides (5px)
+    actualGridLength = gridLength - aBitForPadding - 10;
 
-grid.style.width = gridLength;
-grid.style.height = gridLength;
-grid.style.padding = (aBitForPadding / 2) + "px";
+    // + 'px' is necessary for assigning a pixel value
+    grid.style.width = gridLength + "px";
+    grid.style.height = gridLength + "px";
+    grid.style.padding = (aBitForPadding / 2) + "px";
+}
 
+function initGrid(size = 16) {
+    let grid = document.querySelector('.grid');
+    console.log(actualGridLength);
+
+    let newDivDimensions = actualGridLength/size;
+    let newDiv;
+    for(let i = 0; i < size*size; i++) {
+        newDiv = document.createElement('div');
+        newDiv.style.height = newDiv.style.width = newDivDimensions + "px";
+        newDiv.style.backgroundColor = "azure";
+        newDiv.style.border = "solid 1px lightgray";
+        grid.appendChild(newDiv);
+    }
+}
+
+//actualGridLength is gridLength without margin, border and padding. For new divs
+let actualGridLength;
+setGridSizeOnLoad();
+initGrid();
 /*
 let body = document.querySelector('body');
 body.style.width = "";
